a9909da42a3906d8bd709597d26e1c2473accde2
MFuentes01 <megax.yt.uy@gmail.com>
2023-10-02 11:03:49 -0300

    P1_y_3_Entrega_5

--- js/cart.js
+++ js/cart.js
@@ -1,2 +1,31 @@
-const tabla = document.getElementById("carro")
+const divcarro = document.getElementById("carro");
+const url = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
+const tablaTitulos = document.getElementById("tablaTitulos");
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    data.articles.forEach(article => {
+      const tableRow = document.createElement("tr");
+      tablaTitulos.innerHTML += `
+        <th><img src="${article.image}" id="cartproductimg" class="cartproductimg"></th>
+        <th>${article.name}</th>
+        <th>${article.currency + " " + article.unitCost}</th>
+        <th><input type="number" class="inputCant"></th>
+        <th class="totalPrice"></th>
+      `;
+
+      const input = tablaTitulos.querySelector(".inputCant");
+      const totalPrice = tablaTitulos.querySelector(".totalPrice");
+
+      input.addEventListener("input", function () {
+        const cant = input.value;
+        const totalPriceValue = article.currency + " " + cant * article.unitCost;
+        totalPrice.textContent = totalPriceValue;
+      });
+
+      divcarro.appendChild(tablaTitulos);
+    });
+  });
+
 
