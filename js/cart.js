f2a0c1444ee300f871363280a8eee6e5da2540e9
facundodipaulo <141045729+facundodipaulo@users.noreply.github.com>
2023-10-12 12:03:08 -0300

    Estética y funcionalidad

--- js/cart.js
+++ js/cart.js
@@ -20,7 +20,9 @@ fetch(url)
         <th><img src="${article.image}" id="cartproductimg" class="cartproductimg"></th>
         <th>${article.name}</th>
         <th>${article.currency + " " + article.unitCost}</th>
-        <th><input type="number" class="inputCant"></th>
+        <th class="cuadradito">
+        <input type="number" class="inputCant">
+        </th>
         <th class="totalPrice"></th>
         <tr>
         <th><img src="${nuevoProductoImagen}" id="cartproductimg" class="cartproductimg"></th>
@@ -54,4 +56,22 @@ fetch(url)
     });
   });
 
+//Se crea una constante que referencia al elemento "cerrar_sesion"
+const cerrar_sesion = document.getElementById("cerrar_sesion");
+const usuario = window.localStorage.getItem("usuario"); // Traemos el valor de usuario
 
+console.log(usuario);
+
+//Se crea un evento al hacer click que elimina la información de usuario del localStorage y redirige al usuario a login.html
+cerrar_sesion.addEventListener("click", function () {
+    sessionStorage.removeItem('usuario');
+    window.location.href = "login.html"
+})
+
+
+const datos = document.getElementById("data_user");
+if (!sessionStorage.getItem('isLoggedIn')) {
+    window.location.href = 'login.html'; // Redirigir al login.html si no ha iniciado sesión
+  } else {
+    datos.innerHTML = usuario;
+  };
