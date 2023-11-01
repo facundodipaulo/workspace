8083150f0c42b49cc9f96e230088eabde0417df7
milagros moles <milimoles37@gmail.com>
2023-11-01 18:58:16 -0300

    loginDarkMode

--- js/login.js
+++ js/login.js
@@ -13,8 +13,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-    
-
+    var icon =  document.getElementById("icon");
+    icon.onclick = function(){
+        document.body.classList.toggle("dark-theme");
+        if(document.body.classList.contains("dark-theme")){
+            icon.src = "img/sun.png";
+        } else {
+            icon.src = "img/moon.png";
+        }
+    }
     
     
     function iniciarSesion() {
@@ -54,6 +61,8 @@ document.addEventListener("DOMContentLoaded", function() {
             recordar = false;
         }
     })
-    
+
+
 });
+
 
