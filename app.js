0da3a1fc0a90e96b487cdbe12f38e18a9f2ba4f8
MFuentes01 <megax.yt.uy@gmail.com>
2023-11-13 10:40:08 -0300

    http://localhost:3000/api/subcarpeta/archivo

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express');
-const app = express();
-const puerto = 3000;
+const fs = require('fs');
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.send('Buenaaaaaaaaaaaaaaaaaaaaaaaaaas');
+const app = express();
+const PORT = 3000;
+
+// Middleware para parsear el cuerpo de las solicitudes
+app.use(express.json());
+
+// Ruta para obtener un archivo JSON específico
+app.get('/api/:subfolder/:filename', (req, res) => {
+  const subfolder = req.params.subfolder;
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, 'emercado_api', subfolder, filename);
+
+  // Verificar si el archivo existe
+  if (fs.existsSync(filePath)) {
+    // Leer el archivo y enviarlo como respuesta
+    const jsonData = fs.readFileSync(filePath, 'utf-8');
+    res.json(JSON.parse(jsonData));
+  } else {
+    // Si el archivo no existe, devolver un error 404
+    res.status(404).json({ error: 'Archivo no encontrado' });
+  }
 });
 
-app.listen(puerto, () => {
-    console.log(`Servidor escuchando en http://localhost:${puerto}`);
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
