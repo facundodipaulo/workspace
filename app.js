989788cf347ee0a3bfe6c780a12f38d4ac0f320b
facundodipaulo <141045729+facundodipaulo@users.noreply.github.com>
2023-11-19 02:48:56 -0300

    Cambios_totales

--- app.js
+++ app.js
@@ -1,46 +1,83 @@
 const express = require('express');
-const cors = require('cors'); // Importar el paquete cors
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = 3000;
+const SECRET_KEY = 'your-secret-key'; // Cambia esto con tu clave secreta
 
-// Middleware para habilitar CORS
 app.use(cors());
-
-// Middleware para parsear el cuerpo de las solicitudes
 app.use(express.json());
+
+// Servir archivos estáticos desde las carpetas 'js', 'css', 'img', 'webfonts' y 'emercado_api'
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/img', express.static(path.join(__dirname, 'img')));
+app.use('/webfonts', express.static(path.join(__dirname, 'webfonts')));
+app.use('/emercado_api', express.static(path.join(__dirname, 'emercado_api')));
+
+// Ruta para obtener un archivo HTML específico
+app.get('/:filename.html', (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, `${filename}.html`);
+
+  if (fs.existsSync(filePath)) {
+    res.sendFile(filePath);
+  } else {
+    res.status(404).json({ error: 'Archivo no encontrado' });
+  }
+});
 
 // Ruta para obtener un archivo JSON específico o una lista de archivos en la subcarpeta
 app.get('/api/:subfolder/:filename?', (req, res) => {
   const subfolder = req.params.subfolder;
   const filename = req.params.filename;
 
-  // Construir la ruta de la subcarpeta
   const subfolderPath = path.join(__dirname, 'emercado_api', subfolder);
 
-  // Verificar si se proporcionó un nombre de archivo
   if (filename) {
     const filePath = path.join(subfolderPath, filename);
 
-    // Verificar si el archivo existe
     if (fs.existsSync(filePath)) {
-      // Leer el archivo y enviarlo como respuesta
       const jsonData = fs.readFileSync(filePath, 'utf-8');
       res.json(JSON.parse(jsonData));
     } else {
-      // Si el archivo no existe, devolver un error 404
       res.status(404).json({ error: 'Archivo no encontrado' });
     }
   } else {
-    // Si no se proporcionó un nombre de archivo, obtener la lista de archivos en la subcarpeta
     const files = fs.readdirSync(subfolderPath);
     res.json({ files });
   }
 });
 
-// Iniciar el servidor
+// Nuevo endpoint para autenticación y generación de token
+app.post('/login', (req, res) => {
+  const { user, password } = req.body;
+
+  // Ejemplo simple de autenticación (reemplázalo con tu lógica real)
+  if (user === 'ElonMusk@god.com' && password === 'contraseña') {
+    // Autenticación exitosa, genera un token
+    const token = jwt.sign({ user }, SECRET_KEY, { expiresIn: '1h' }); // El token expirará en 1 hora
+
+    res.json({ token, user });
+  } else {
+    res.status(401).json({ error: 'Credenciales incorrectas' });
+  }
+});
+
+// Ruta para acceder al archivo index.html dentro de la carpeta global 'workspace'
+app.get('/index', (req, res) => {
+  const indexPath = path.join(__dirname, 'index.html');
+
+  if (fs.existsSync(indexPath)) {
+    res.sendFile(indexPath);
+  } else {
+    res.status(404).json({ error: 'Archivo no encontrado' });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
--- js/login.js
+++ js/login.js
@@ -1,100 +1,76 @@
-document.addEventListener("DOMContentLoaded", function() {
-    //Se declaran las constantes
+document.addEventListener("DOMContentLoaded", function () {
     const alertContainer = document.getElementById('alert-container');
     const button = document.getElementById('button');
     const checkbox = document.getElementById('checkbox');
     const campoUsuario = document.getElementById('user');
     const campoContrasena = document.getElementById('password');
     let recordar = false;
-    let dataRecordar = localStorage.getItem('recordar');
-    let usuarioGuardado = localStorage.getItem('usuario');
-    
-
-    // Función para recordar datos de usuario.
-    if (dataRecordar === "true" && usuarioGuardado) {
-        console.log(usuarioGuardado);
-        campoUsuario.value = usuarioGuardado;
-    } else {
-        console.log("No hay usuario recordado");
-        campoUsuario.value.delete;
-    }
-
+  
     function showAlert(message, success = false) {
-        const alertElement = document.createElement("div");
-        alertElement.className = `alert ${success ? 'alert-success' : 'alert-danger'}`;
-        alertElement.textContent = message;
-        alertContainer.appendChild(alertElement);
-      
-        // Limpia el mensaje después de unos segundos
-        setTimeout(() => {
-          alertContainer.removeChild(alertElement);
-        }, 5000);
-      }
-
-    // Función para cambiar Modo Día a Modo Noche y el Ícono de Luna a Sol.
-    var icon =  document.getElementById("icon");
-    icon.onclick = function(){
-        document.body.classList.toggle("dark-theme");
-        if(document.body.classList.contains("dark-theme")){
-            icon.src = "img/sun.png";
-        } else {
-            icon.src = "img/moon.png";
-        }
+      const alertElement = document.createElement("div");
+      alertElement.className = `alert ${success ? 'alert-success' : 'alert-danger'}`;
+      alertElement.textContent = message;
+      alertContainer.appendChild(alertElement);
+  
+      setTimeout(() => {
+        alertContainer.removeChild(alertElement);
+      }, 5000);
     }
-    
+  
     function iniciarSesion() {
-        // Se declaran las constantes.
-        const usuario = campoUsuario.value;
-        const contrasena = campoContrasena.value;
-
-        // Previene continuar = campo vacío
-        if (usuario === "" || contrasena === "") {
-            showAlert("Por favor, complete todos los campos.", false);
-        } else if (usuario.indexOf("@") === -1) {
-            showAlert("El Usuario debe contener el carácter @.", false);
-        } else if (contrasena.length < 6) {
-            showAlert("La contraseña debe tener al menos 6 caracteres.", false);
-        } else {
-            // Verifica si la casilla de recordar contraseña no está marcada
-            if (!checkbox.checked) {
-                // Si no está marcada, limpia los campos de usuario y contraseña
-                campoUsuario.value = '';
-                campoContrasena.value = '';
+      const usuario = campoUsuario.value;
+      const contrasena = campoContrasena.value;
+  
+      if (usuario === "" || contrasena === "") {
+        showAlert("Por favor, complete todos los campos.", false);
+      } else if (usuario.indexOf("@") === -1) {
+        showAlert("El Usuario debe contener el carácter @.", false);
+      } else if (contrasena.length < 6) {
+        showAlert("La contraseña debe tener al menos 6 caracteres.", false);
+      } else {
+        fetch('http://localhost:3000/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ user: usuario, password: contrasena }),
+        })
+          .then(response => response.json())
+          .then(data => {
+            if (data.token) {
+              localStorage.setItem('usuario', data.user); // Almacena el usuario en localStorage
+              window.location.href = "index.html";
+            } else {
+              console.error('Error de autenticación:', data.error);
+              showAlert("Credenciales incorrectas. Por favor, inténtelo de nuevo.", false);
             }
-
-            // Redirige al usuario a la página especificada después de la autenticación exitosa
-            sessionStorage.setItem('isLoggedIn', 'true');
-            window.localStorage.setItem('usuario', usuario);
-            window.localStorage.setItem('recordar', recordar);
-            console.log(recordar);
-            window.location.href = "index.html";
-        }
+          })
+          .catch(error => {
+            console.error('Error en la solicitud:', error);
+          });
+      }
     }
-    
-    button.addEventListener("click", function(e) {
-        e.preventDefault(); // Evita el envío del formulario por defecto
-        iniciarSesion();
+  
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      iniciarSesion();
     });
-
-    checkbox.addEventListener('change', function() {
-        if (checkbox.checked) {
-            recordar = true;
-        } else {
-            recordar = false;
-        }
+  
+    checkbox.addEventListener('change', function () {
+      recordar = checkbox.checked;
     });
-
-    //mostrar contraseña
+  
     const passwordField = document.getElementById('password');
     const showPasswordButton = document.getElementById('mostrar_contraseña');
-
-    showPasswordButton.addEventListener('click', function() {
-        if (passwordField.type === 'password') {
-            passwordField.type = 'text';
-            showPasswordButton.innerHTML = '<i class="far fa-eye-slash"></i>';
-        } else {
-            passwordField.type = 'password';
-            showPasswordButton.innerHTML = '<i class="far fa-eye"></i>';
-        }
+  
+    showPasswordButton.addEventListener('click', function () {
+      if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+        showPasswordButton.innerHTML = '<i class="far fa-eye-slash"></i>';
+      } else {
+        passwordField.type = 'password';
+        showPasswordButton.innerHTML = '<i class="far fa-eye"></i>';
+      }
     });
-});
+  });
+  
